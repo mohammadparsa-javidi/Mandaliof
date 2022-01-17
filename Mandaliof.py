287241b48b88603ac59b4bbc84cf992792373496
mohammadparsa-javidi <85730592+mohammadparsa-javidi@users.noreply.github.com>
2022-01-17 22:13:19 +0330

    Add files via upload

---
+++ Mandaliof.py
@@ -0,0 +1,352 @@
+from tkinter import *
+win = Tk()
+win.geometry("500x500")
+win.title("Mandaliof")
+def get_info():
+    onsor = entry_onsor.get()
+    if onsor == "H" or onsor == "h":
+        #print("Dore : 1"+"\n"+"groh : 1")
+        label_info.configure(text="Dore : 1"+"\n"+"groh : 1",font=(12),fg="green")
+
+    if onsor == "Li" or onsor == "li":
+        #print("Dore : 2"+"\n"+"groh : 1")
+        label_info.configure(text="Dore : 2"+"\n"+"groh : 1",font=(12),fg="green")
+
+    if onsor == "Na" or onsor == "na":
+        #print("Dore : 3"+"\n"+"groh : 1")
+        label_info.configure(text="Dore : 3"+"\n"+"groh : 1",font=(12),fg="green")
+
+    if onsor == "K" or onsor == "k":
+        #print("Dore : 4"+"\n"+"groh : 1")
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 1",font=(12),fg="green")
+
+    if onsor == "Rb" or onsor == "rb":
+        #print("Dore : 5"+"\n"+"groh : 1")
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 1",font=(12),fg="green")
+
+    if onsor == "Cs" or onsor == "cs":
+        #print("Dore : 6"+"\n"+"groh : 1")
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 1",font=(12),fg="green")
+
+    if onsor == "Fr" or onsor == "fr":
+       #print("Dore : 7"+"\n"+"groh : 1")
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 1",font=(12),fg="green")
+
+    if onsor == "Be" or onsor == "be":
+       #print("Dore : 2"+"\n"+"groh : 2")
+        label_info.configure(text="Dore : 2"+"\n"+"groh : 2",font=(12),fg="green")
+
+    if onsor == "Mg" or onsor == "mg":
+        #print("Dore : 3"+"\n"+"groh : 2")
+        label_info.configure(text="Dore : 3"+"\n"+"groh : 2",font=(12),fg="green")
+
+    if onsor == "Ca" or onsor == "ca":
+        #print("Dore : 4"+"\n"+"groh : 2")
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 2",font=(12),fg="green")
+        
+
+    if onsor == "Sr" or onsor == "sr":
+        #print("Dore : 5"+"\n"+"groh : 2")
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 2",font=(12),fg="green")
+
+    if onsor == "Ba" or onsor == "ba":
+        #print("Dore : 6"+"\n"+"groh : 2")
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 2",font=(12),fg="green")
+
+    if onsor == "Ra" or onsor == "ra":
+        #print("Dore : 7"+"\n"+"groh : 2")
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 2",font=(12),fg="green")
+
+    if onsor == "Sc" or onsor == "sc":
+        #print("Dore : 4"+"\n"+"groh : 3")
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 3",font=(12),fg="green")
+
+    if onsor == "Y" or onsor == "y":
+        #print("Dore : 5"+"\n"+"groh : 3")
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 3",font=(12),fg="green")
+
+    if onsor == "Lu" or onsor == "lu":
+        #print("Dore : 6"+"\n"+"groh : 3")
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 3",font=(12),fg="green")
+    
+    if onsor == "Lr" or onsor == "lr":
+        #print("Dore : 7"+"\n"+"groh : 3")
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 3",font=(12),fg="green")
+
+    if onsor == "Ti" or onsor == "ti":
+        #print("Dore : 4"+"\n"+"groh : 4")
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 4",font=(12),fg="green")
+
+    if onsor == "Zr" or onsor == "zr":
+        #print("Dore : 5"+"\n"+"groh : 4")
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 4",font=(12),fg="green")
+    
+    if onsor == "Hf" or onsor == "hf":
+        #print("Dore : 6"+"\n"+"groh : 4")
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 4",font=(12),fg="green")
+
+    if onsor == "Rf" or onsor == "rf":
+        #print("Dore : 7"+"\n"+"groh : 4")
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 4",font=(12),fg="green")
+
+    if onsor == "V" or onsor == "v":
+        #print("Dore : 4"+"\n"+"groh : 5")
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 5",font=(12),fg="green")
+
+    if onsor == "Nb" or onsor == "nb":
+        #print("Dore : 5"+"\n"+"groh : 5")
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 5",font=(12),fg="green")
+
+    if onsor == "Ta" or onsor == "ta":
+        #print("Dore : 6"+"\n"+"groh : 5")
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 5",font=(12),fg="green")
+
+    if onsor == "Ta" or onsor == "ta":
+        #print("Dore : 6"+"\n"+"groh : 5")
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 5",font=(12),fg="green")
+
+    if onsor == "Db" or onsor == "db":
+        #print("Dore : 7"+"\n"+"groh : 5")
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 5",font=(12),fg="green")
+
+    if onsor == "Cr" or onsor == "cr":
+        #print("Dore : 4"+"\n"+"groh : 6")
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 6",font=(12),fg="green")
+
+    if onsor == "Cr" or onsor == "cr":
+        #print("Dore : 4"+"\n"+"groh : 6")
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 6",font=(12),fg="green")
+
+    if onsor == "Mo" or onsor == "mo":
+        #print("Dore : 5"+"\n"+"groh : 6")
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 6",font=(12),fg="green")
+
+    if onsor == "W" or onsor == "w":
+        #print("Dore : 6"+"\n"+"groh : 6")
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 6",font=(12),fg="green")
+
+    if onsor == "Sg" or onsor == "sg":
+        #print("Dore : 7"+"\n"+"groh : 6")
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 6",font=(12),fg="green")
+
+    if onsor == "Mn" or onsor == "mn":
+        #print("Dore : 4"+"\n"+"groh : 7")
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 7",font=(12),fg="green")
+
+    if onsor == "Tc" or onsor == "tc":
+        #print("Dore : 5"+"\n"+"groh : 7")
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 7",font=(12),fg="green")
+    
+    if onsor == "Re" or onsor == "re":
+        #print("Dore : 6"+"\n"+"groh : 7")
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 7",font=(12),fg="green")
+
+    if onsor == "Bh" or onsor == "bh":
+        #print("Dore : 7"+"\n"+"groh : 7")
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 7",font=(12),fg="green")
+
+    if onsor == "Fe" or onsor == "fe":
+        #print("Dore : 4"+"\n"+"groh : 8")
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 8",font=(12),fg="green")
+    
+
+    if onsor == "Ru" or onsor == "ru":
+        #print("Dore : 5"+"\n"+"groh : 8")
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 8",font=(12),fg="green")
+        
+
+    if onsor == "Os" or onsor == "os":
+        #print("Dore : 6"+"\n"+"groh : 8")
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 8",font=(12),fg="green")
+
+    if onsor == "Hs" or onsor == "hs":
+        #print("Dore : 7"+"\n"+"groh : 8")
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 8",font=(12),fg="green")
+    
+    if onsor == "Co" or onsor == "co":
+        #print("Dore : 4"+"\n"+"groh : 9")
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 9",font=(12),fg="green")
+
+    if onsor == "Rh" or onsor == "rh":
+        #print("Dore : 5"+"\n"+"groh : 9")
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 9",font=(12),fg="green")
+
+    if onsor == "Ir" or onsor == "ir":
+        #print("Dore : 6"+"\n"+"groh : 9")
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 9",font=(12),fg="green")
+
+    if onsor == "Mt" or onsor == "mt":
+        #print("Dore : 7"+"\n"+"groh : 9")
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 9",font=(12),fg="green")
+        
+    if onsor == "Ni" or onsor == "ni":
+        #print("Dore : 4"+"\n"+"groh : 10")
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 10",font=(12),fg="green")
+
+    if onsor == "Pd" or onsor == "pd":
+        #print("Dore : 5"+"\n"+"groh : 10")
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 10",font=(12),fg="green")
+
+    if onsor == "Pt" or onsor == "pt":
+        #print("Dore : 6"+"\n"+"groh : 10")
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 10",font=(12),fg="green")
+    
+    if onsor == "Ds" or onsor == "ds":
+        #print("Dore : 7"+"\n"+"groh : 10")
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 10",font=(12),fg="green")
+
+    if onsor == "Cu" or onsor == "cu":
+        #print("Dore : 4"+"\n"+"groh : 11")
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 11",font=(12),fg="green")
+
+    if onsor == "Ag" or onsor == "ag":
+        #print("Dore : 5"+"\n"+"groh : 11")
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 11",font=(12),fg="green")
+
+    if onsor == "Au" or onsor == "au":
+        #print("Dore : 6"+"\n"+"groh : 11")
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 11",font=(12),fg="green")
+
+    if onsor == "Rg" or onsor == "rg":
+        #print("Dore : 7"+"\n"+"groh : 11")
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 11",font=(12),fg="green")
+
+    if onsor == "Zn" or onsor == "zn":
+        #print("Dore : 4"+"\n"+"groh : 12")
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 12",font=(12),fg="green")
+
+    if onsor == "Cd" or onsor == "cd":
+        #print("Dore : 5"+"\n"+"groh : 12")
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 12",font=(12),fg="green")
+
+    if onsor == "Hg" or onsor == "hg":
+        #print("Dore : 6"+"\n"+"groh : 12")
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 12",font=(12),fg="green")
+
+    if onsor == "Cn" or onsor == "cn":
+        #print("Dore : 7"+"\n"+"groh : 12")
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 12",font=(12),fg="green")
+        
+    if onsor == "B" or onsor == "b":
+        label_info.configure(text="Dore : 2"+"\n"+"groh : 13",font=(12),fg="green")
+
+    if onsor == "Al" or onsor == "al":
+        label_info.configure(text="Dore : 3"+"\n"+"groh : 13",font=(12),fg="green")
+    
+    if onsor == "Ga" or onsor == "ga":
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 13",font=(12),fg="green")
+
+    if onsor == "In" or onsor == "in":
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 13",font=(12),fg="green")
+
+    if onsor == "Ti" or onsor == "ti":
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 13",font=(12),fg="green")
+    
+    if onsor == "Nh" or onsor == "nh":
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 13",font=(12),fg="green")
+
+    if onsor == "C" or onsor == "c":
+        label_info.configure(text="Dore : 2"+"\n"+"groh : 14",font=(12),fg="green")
+    
+    if onsor == "Si" or onsor == "si":
+        label_info.configure(text="Dore : 3"+"\n"+"groh : 14",font=(12),fg="green")
+
+    if onsor == "Ge" or onsor == "ge":
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 14",font=(12),fg="green")
+    
+    if onsor == "Sn" or onsor == "sn":
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 14",font=(12),fg="green")
+
+    if onsor == "Pb" or onsor == "pb":
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 14",font=(12),fg="green")
+
+    if onsor == "Fl" or onsor == "fl":
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 14",font=(12),fg="green")
+
+    if onsor == "N" or onsor == "n":
+        label_info.configure(text="Dore : 2"+"\n"+"groh : 15",font=(12),fg="green")
+
+    if onsor == "P" or onsor == "p":
+        label_info.configure(text="Dore : 3"+"\n"+"groh : 15",font=(12),fg="green")
+
+    if onsor == "As" or onsor == "as":
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 15",font=(12),fg="green")
+
+    if onsor == "Sb" or onsor == "sb":
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 15",font=(12),fg="green")
+
+    if onsor == "Bi" or onsor == "bi":
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 15",font=(12),fg="green")
+
+    if onsor == "Mc" or onsor == "mc":
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 15",font=(12),fg="green")
+
+    if onsor == "O" or onsor == "o":
+        label_info.configure(text="Dore : 2"+"\n"+"groh : 16",font=(12),fg="green")
+
+    if onsor == "S" or onsor == "s":
+        label_info.configure(text="Dore : 3"+"\n"+"groh : 16",font=(12),fg="green")
+
+    if onsor == "Se" or onsor == "se":
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 16",font=(12),fg="green")
+
+    if onsor == "Te" or onsor == "te":
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 16",font=(12),fg="green")
+    
+    if onsor == "Po" or onsor == "po":
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 16",font=(12),fg="green")
+    
+    if onsor == "Lv" or onsor == "lv":
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 16",font=(12),fg="green")
+
+    if onsor == "F" or onsor == "f":
+        label_info.configure(text="Dore : 2"+"\n"+"groh : 17",font=(12),fg="green")
+
+    if onsor == "Cl" or onsor == "cl":
+        label_info.configure(text="Dore : 3"+"\n"+"groh : 17",font=(12),fg="green")
+
+    if onsor == "Br" or onsor == "br":
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 17",font=(12),fg="green")
+
+    if onsor == "I" or onsor == "i":
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 17",font=(12),fg="green")
+
+    if onsor == "At" or onsor == "at":
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 17",font=(12),fg="green")
+
+    if onsor == "Ts" or onsor == "ts":
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 17",font=(12),fg="green")
+
+    if onsor == "He" or onsor == "he":
+        label_info.configure(text="Dore : 1"+"\n"+"groh : 18",font=(12),fg="green")
+
+    if onsor == "Ne" or onsor == "ne":
+        label_info.configure(text="Dore : 2"+"\n"+"groh : 18",font=(12),fg="green")
+
+    if onsor == "Ar" or onsor == "ar":
+        label_info.configure(text="Dore : 3"+"\n"+"groh : 18",font=(12),fg="green")
+
+    if onsor == "Kr" or onsor == "kr":
+        label_info.configure(text="Dore : 4"+"\n"+"groh : 18",font=(12),fg="green")
+
+    if onsor == "Xe" or onsor == "xe":
+        label_info.configure(text="Dore : 5"+"\n"+"groh : 18",font=(12),fg="green")
+
+    if onsor == "Rn" or onsor == "rn":
+        label_info.configure(text="Dore : 6"+"\n"+"groh : 18",font=(12),fg="green")
+
+    if onsor == "Og" or onsor == "og":
+        label_info.configure(text="Dore : 7"+"\n"+"groh : 18",font=(12),fg="green")
+
+    
+
+entry_onsor = Entry(win,bd=1)
+entry_onsor.pack()
+btn_onsor = Button(win,text="دریافت اطلاعات عنصر",command=get_info)
+btn_onsor.pack()
+
+label_info = Label(win,text="")
+label_info.pack()
+
+
+win.mainloop()
